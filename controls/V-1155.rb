6f369146125a29c2ca4934c65454dbedf37d6e69
Jared Burns <burns.jared@gmail.com>
2020-03-31 16:44:30 -0400

    Updated S-1-21 to be S-1-5-21
    Signed-off-by: Jared Burns <burns.jared@gmail.com>

--- controls/V-1155.rb
+++ controls/V-1155.rb
@@ -94,10 +94,10 @@ control 'V-1155' do
     #get_domain_sid = command('wmic useraccount get sid | FINDSTR /V SID | Select -First 2').stdout.strip
     domain_sid = input('domain_sid')
     describe security_policy do
-      its('SeDenyNetworkLogonRight') { should include "S-1-21-#{domain_sid}-512" }
+      its('SeDenyNetworkLogonRight') { should include "S-1-5-21-#{domain_sid}-512" }
     end
     describe security_policy do
-      its('SeDenyNetworkLogonRight') { should include "S-1-21-#{domain_sid}-519" }
+      its('SeDenyNetworkLogonRight') { should include "S-1-5-21-#{domain_sid}-519" }
     end
   end
 end
